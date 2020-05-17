d9c57fcd7981033f46ad183ea3db3528aa4a743c
Marvin von Hagen <M@vHagen.me>
2020-05-17 11:38:25 +0200

    extracted AR functionality

--- Good Design.playgroundbook/Contents/Chapters/GoodDesign.playgroundchapter/Pages/Principles 3 & 4.playgroundpage/Contents.swift
+++ Good Design.playgroundbook/Contents/Chapters/GoodDesign.playgroundchapter/Pages/Principles 3 & 4.playgroundpage/Contents.swift
@@ -11,14 +11,12 @@ enum Version {
 
 func showAppleLogo(version: Version) {
     switch version {
-    case .firstLogo:
+    case .artLogo:
         _setup(imgSet: 21)
     case .rainbowLogo:
         _setup(imgSet: 22)
-    case .artLogo:
-        _setup(imgSet: 23)
     default:
-        _setup(imgSet: 10)
+        _setup(imgSet: 404)
     }
 }
 //#-end-hidden-cod
@@ -37,33 +35,4 @@ func showAppleLogo(version: Version) {
  If you want to see a random art logo, call `showAppleLogo(version:)` with `.artLogo` as the version parameter. To see the first apple logo ever or the legendary rainbow logo, use `.firstLogo` and `.rainbowLogo` respectively 🍎
 
  */
-//showAppleLogo(version: ./*#-editable-code*/<#T##Apple Logo Type##String#>/*#-end-editable-code*/)
-
-
-
-let arView = ARView(frame: CGRect(x: 0, y: 0, width: 800, height: 200), cameraMode: .ar, automaticallyConfigureSession: true)
-
-let fileURL = Bundle.main.url(forResource: "artLogosAR", withExtension: "reality")
-let scene = try! Entity.load(contentsOf: fileURL!)
-
-let anchor = AnchorEntity()
-anchor.addChild(scene)
-anchor.scale = [1,1,1]
-
-func degreesToRadians(_ degrees: Float) -> Float {
-    return degrees * .pi / 180
-}
-
-let originVector = simd_float3(x: 0, y: 0, z: -1)
-
-let quaternion = simd_quatf(angle: degreesToRadians(90),
-                            axis: simd_float3(x: 1,
-                                              y: 0,
-                                              z: 0))
-
-anchor.setOrientation(quaternion, relativeTo: nil)
-anchor.setPosition(originVector, relativeTo: nil)
-arView.scene.anchors.append(anchor)
-
-
-PlaygroundPage.current.liveView = arView
+showAppleLogo(version: ./*#-editable-code*/<#T##Apple Logo Type##String#>/*#-end-editable-code*/)
--- Good Design.playgroundbook/Contents/Sources/PlaygroundInternal/Setup.swift
+++ Good Design.playgroundbook/Contents/Sources/PlaygroundInternal/Setup.swift
@@ -3,6 +3,48 @@ import PlaygroundSupport
 
 
 public func _setup(imgSet: Int) {
-    let viewController = ImageViewController(imgSet: imgSet)
-    PlaygroundPage.current.liveView = viewController
+    if imgSet > 21 {
+        let viewController = ImageViewController(imgSet: imgSet)
+        PlaygroundPage.current.liveView = viewController
+        return
+    }
+    
+    var fileName: String
+    
+    switch imgSet {
+    case 11:
+        fileName = "Android Fragmentation"
+    case 12:
+        fileName = "iPhone Fragmentation"
+    case 21:
+        fileName = "artLogosAR"
+    default:
+        return
+    }
+    
+    let arView = ARView(frame: CGRect(x: 0, y: 0, width: 800, height: 200), cameraMode: .ar, automaticallyConfigureSession: true)
+    
+    let fileURL = Bundle.main.url(forResource: fileName, withExtension: "reality")
+    let scene = try! Entity.load(contentsOf: fileURL!)
+    
+    let anchor = AnchorEntity()
+    anchor.addChild(scene)
+    anchor.scale = [1,1,1]
+    
+    func degreesToRadians(_ degrees: Float) -> Float {
+        return degrees * .pi / 180
+    }
+    
+    let originVector = simd_float3(x: 0, y: 0, z: -1)
+    
+    let quaternion = simd_quatf(angle: degreesToRadians(90),
+                                axis: simd_float3(x: 1,
+                                                  y: 0,
+                                                  z: 0))
+    
+    anchor.setOrientation(quaternion, relativeTo: nil)
+    anchor.setPosition(originVector, relativeTo: nil)
+    arView.scene.anchors.append(anchor)
+    
+    PlaygroundPage.current.liveView = arView
 }
